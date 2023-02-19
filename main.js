34546a761d086b6b5f8f5d2e570b5e7f88ddb514
DESKTOP-EDQ2DIM\Tomino <tomas.krouzel.jr@seznam.cz>
2023-02-19 19:23:00 +0100

    czechitas-l3-du

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-// tady je místo pro náš program
+let celeJmeno = prompt("Jaké je vaše jméno a příjmení?:");
+let vek = Number(prompt("Jaký je váš věk?:"));
+let barva = prompt("Oblíbená barva - anglicky");
+
+document.body.innerHTML += "<p>" + celeJmeno + ", vek: " + vek + "<p>";
+document.body.style.color = barva;
 
 
 
